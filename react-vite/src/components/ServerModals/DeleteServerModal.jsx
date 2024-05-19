7a967ae573bd847ed04b7c377d6c04e0f503a302
Robert Stomski <stomski.robert@gmail.com>
2024-05-19 13:11:16 -0600

    delete server modal pretty well built out

---
+++ react-vite/src/components/ServerModals/DeleteServerModal.jsx
@@ -0,0 +1,38 @@
+import { useDispatch } from "react-redux";
+import { useModal } from "../../context/Modal";
+import { deleteServerThunk } from "../../store/servers";
+// import "./DeleteServerModal.css";
+
+function DeleteServerModal({ serverId }) {
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await dispatch(deleteServerThunk(serverId));
+    closeModal();
+    return;
+  };
+
+  const cancelDelete = (e) => {
+    e.preventDefault();
+    dispatch(closeModal());
+  };
+
+  return (
+    <div className="delete-modal-container">
+      <div className="form-container">
+        <h1 id="modaltitles">Are You Sure?</h1>
+
+        <button type="submit" onClick={handleSubmit} className="delete">
+          yes, delete
+        </button>
+        <button type="submit" onClick={cancelDelete} className="cancel">
+          Go Back
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default DeleteServerModal;
