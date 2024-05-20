cc3e875221ac686a1e421b92560783c82e530db9
Robert Stomski <stomski.robert@gmail.com>
2024-05-20 12:12:30 -0600

    seed data for some reason on this vranch

---
+++ app/seeds/channels.py
@@ -0,0 +1,60 @@
+from app.models import db, Channel, environment, SCHEMA
+from sqlalchemy.sql import text
+
+def seed_channels():
+    channels = [
+        Channel(
+            name="General",
+            serverId=1,
+            creatorId=1,
+
+        ),
+        Channel(
+            name="General",
+            serverId=2,
+            creatorId=2,
+
+        ),
+        Channel(
+            name="General",
+            serverId=3,
+            creatorId=3,
+
+        ),
+        Channel(
+            name="General",
+            serverId=4,
+            creatorId=1,
+
+        ),
+        Channel(
+            name="General",
+            serverId=5,
+            creatorId=2,
+
+        ),
+        Channel(
+            name="Mine craft... more like YOUR craft",
+            serverId=1,
+            creatorId=1,
+
+        ),
+
+        Channel(
+            name="minecraft unionization effort",
+            serverId=1,
+            creatorId=1,
+
+        ),
+    ]
+
+
+    db.session.add_all(channels)
+    db.session.commit()
+
+def undo_servers():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.channels RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM channels"))
+    db.session.commit()
