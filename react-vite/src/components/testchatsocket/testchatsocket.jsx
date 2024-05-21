6599af1ab7a7faf9dd98bdac5cc10545aa3ebfec
Robert Stomski <stomski.robert@gmail.com>
2024-05-21 17:41:03 -0600

    render chat test

--- react-vite/src/components/testchatsocket/testchatsocket.jsx
+++ react-vite/src/components/testchatsocket/testchatsocket.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { io } from "socket.io-client";
+import "./testchatsocket.css";
 let socket;
 
 const Chat = () => {
@@ -10,16 +11,23 @@ const Chat = () => {
 
   useEffect(() => {
     // open socket connection
-    // create websocket
-    socket = io();
+    // create websockets
+    if (import.meta.env.MODE === "production") {
+      url = "https://babbl.onrender.com";
+    } else {
+      url = "http://127.0.0.1:8000";
+    }
+
+    socket = io(url);
+    console.log("%c socket log>", "color:blue; font-size: 26px", socket);
 
     socket.on("chat", (chat) => {
       setMessages((messages) => [...messages, chat]);
     });
     // when component unmounts, disconnect
-    return () => {
-      socket.disconnect();
-    };
+    // return () => {
+    //   socket.disconnect();
+    // };
   }, []);
 
   const updateChatInput = (e) => {
@@ -38,7 +46,7 @@ const Chat = () => {
   return (
     user && (
       <div>
-        <div>
+        <div className="socket-messages-scroll">
           {messages.map((message, ind) => (
             <div key={ind}>{`${message.user}: ${message.msg}`}</div>
           ))}
