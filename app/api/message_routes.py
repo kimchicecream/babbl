07c47075195996ab8e03abcf694089be60a6489b
Robert Stomski <stomski.robert@gmail.com>
2024-05-21 00:10:41 -0600

    cleaning up

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -43,3 +43,7 @@ def edit_message_by_id(messageId):
     form = CreateMessageForm()
     if form.validate_on_submit:
         return "time to party"
+
+@message_routes.route('/<int:messageId>/delete')
+def delete_message_by_id(messageId):
+    message = Message.query.
