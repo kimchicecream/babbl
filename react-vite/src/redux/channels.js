e39f857353532877abbe34789118b45c9bdabd11
Robert Stomski <stomski.robert@gmail.com>
2024-05-22 00:23:56 -0600

    now im typong =

--- react-vite/src/redux/channels.js
+++ react-vite/src/redux/channels.js
@@ -1,9 +1,23 @@
 const GET_CHANNELS_BY_SERVER = "channels/getChannelsByServer";
+const CREATE_CHANNEL = "channels/createChannel";
+
+const createChannel = (channel) => ({
+  type: CREATE_CHANNEL,
+  payload: channel,
+});
 
 const getChannelsByServer = (channels) => ({
   type: GET_CHANNELS_BY_SERVER,
   payload: channels,
 });
+
+const createChannelThunk = (channelObj) => async (dispatch) => {
+  const newChannel = await fetch("/api/channels/", {
+    method: "POST",
+    body: channelObj,
+  });
+  dispatch(createChannel(newChannel));
+};
 
 export const getChannelsByServerThunk = (serverId) => async (dispatch) => {
   const serversChannels = await fetch(`api/servers/:${serverId}/channels`);
@@ -16,6 +30,11 @@ const channelsReducer = (state = {}, action) => {
     case GET_CHANNELS_BY_SERVER: {
       newState = { ...state };
       newState.channels = action.payload.channels;
+      return newState;
+    }
+    case CREATE_CHANNEL: {
+      newState = { ...state };
+      newState.channels.channelId = action.payload.channel;
       return newState;
     }
     default:
