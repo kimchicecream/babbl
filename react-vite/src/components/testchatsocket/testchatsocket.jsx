cf6f0ee5d6c9b8efae6ad4b961bf1033426bc50b
cpeter41 <chris.steven.peters@gmail.com>
2024-05-22 22:06:39 -0700

    minor css on channel list

--- react-vite/src/components/testchatsocket/testchatsocket.jsx
+++ react-vite/src/components/testchatsocket/testchatsocket.jsx
@@ -20,26 +20,30 @@ const Chat = ({ initMessages, channelId }) => {
         socket = io(socket_url);
 
         socket.on("chat", (message) => {
-          if (message.channelId === channelId) {
-            setMessages((prevMessages) => [
-              ...prevMessages,
-              {
-                channelId,
-                message: message["msg"],
-                user: { name: user.username, id: user.id, imageUrl: user.imageUrl }
-              }
-            ])
-          }
+            if (message.channelId === channelId) {
+                setMessages((prevMessages) => [
+                    ...prevMessages,
+                    {
+                        channelId,
+                        message: message["msg"],
+                        user: {
+                            name: user.username,
+                            id: user.id,
+                            imageUrl: user.imageUrl,
+                        },
+                    },
+                ]);
+            }
         });
 
         return () => {
-          socket.emit('leave', { channelId });
-          socket.disconnect();
+            socket.emit("leave", { channelId });
+            socket.disconnect();
         };
     }, [channelId, user]);
     useEffect(() => {
         console.log("MESSAGES: ", messages);
-    }, [messages])
+    }, [messages]);
 
     useEffect(() => {
         console.log("init changed: ", messages);
@@ -47,11 +51,12 @@ const Chat = ({ initMessages, channelId }) => {
     }, [initMessages]);
 
     useEffect(() => {
-      // Scroll to the bottom when messages change
-      if (messagesContainerRef.current) {
-          messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
-      }
-  }, [messages]);
+        // Scroll to the bottom when messages change
+        if (messagesContainerRef.current) {
+            messagesContainerRef.current.scrollTop =
+                messagesContainerRef.current.scrollHeight;
+        }
+    }, [messages]);
 
     const updateChatInput = (e) => {
         setChatInput(e.target.value);
@@ -69,50 +74,48 @@ const Chat = ({ initMessages, channelId }) => {
 
     return (
         user && (
-            <div className='chat-socket-container'>
-
-
-
-              <div className="socket-messages-scroll" ref={messagesContainerRef}>
-                <div className="messages-wrapper">
-                  {messages.map((message, ind) => (
-                      <div className="message-container" key={ind}>
-                          <div className="profile-pic-container">
-                              {message?.user?.imageUrl && (
-                                  <img
-                                      src={message.user.imageUrl}
-                                      alt={"profile pic"}
-                                  ></img>
-                              )}
-                          </div>
-                          <div className="username-message-container">
-                              <div className="username-time-container">
-                                  <span className="username">
-                                      {/* {message.username} */}username
-                                  </span>
-                                  <span className="time"></span>
-                              </div>
-                              <div className="message-text">
-                                  <p>{message.message}</p>
-                              </div>
-                          </div>
-                          <div className="message-mamangement-container">
-                              <button className="reactions"></button>
-                              <button className="edit"></button>
-                              <button className="delete"></button>
-                          </div>
-                      </div>
-                  ))}
-                  <div ref={messagesEndRef}></div>
+            <div className="chat-socket-container">
+                <div
+                    className="socket-messages-scroll"
+                    ref={messagesContainerRef}
+                >
+                    <div className="messages-wrapper">
+                        {messages.map((message, ind) => (
+                            <div className="message-container" key={ind}>
+                                <div className="profile-pic-container">
+                                    {message?.user?.imageUrl && (
+                                        <img
+                                            src={message.user.imageUrl}
+                                            alt={"profile pic"}
+                                        ></img>
+                                    )}
+                                </div>
+                                <div className="username-message-container">
+                                    <div className="username-time-container">
+                                        <span className="username">
+                                            {/* {message.username} */}username
+                                        </span>
+                                        <span className="time"></span>
+                                    </div>
+                                    <div className="message-text">
+                                        <p>{message.message}</p>
+                                    </div>
+                                </div>
+                                <div className="message-mamangement-container">
+                                    <button className="reactions"></button>
+                                    <button className="edit"></button>
+                                    <button className="delete"></button>
+                                </div>
+                            </div>
+                        ))}
+                        <div ref={messagesEndRef}></div>
+                    </div>
                 </div>
-              </div>
 
-
-
-              <form className='input-field' onSubmit={sendChat}>
-                  <input value={chatInput} onChange={updateChatInput} />
-                  <button type="submit">Send</button>
-              </form>
+                <form className="input-field" onSubmit={sendChat}>
+                    <input value={chatInput} onChange={updateChatInput} />
+                    <button type="submit">Send</button>
+                </form>
             </div>
         )
     );
