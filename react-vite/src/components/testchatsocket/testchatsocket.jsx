760970abe7bd3db8e679d0e67b13e04713433278
cpeter41 <chris.steven.peters@gmail.com>
2024-05-23 00:47:37 -0700

    reactions halfway done

--- react-vite/src/components/testchatsocket/testchatsocket.jsx
+++ react-vite/src/components/testchatsocket/testchatsocket.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { io } from "socket.io-client";
+import { ReactionsList } from "./reactionsList";
 import "./testchatsocket.css";
 let socket;
 
@@ -11,13 +12,13 @@ const reduceReactions = (reactions) => {
         return counts;
     }, {});
 
-    console.log("COUNTS", counts);
     const buttons = [];
-    for (const [key, value] of Object.entries(counts)) {
+    // map counts to actual reaction buttons
+    for (const [key, count] of Object.entries(counts)) {
         buttons.push(
             <button className="reaction-button">
                 {/* TODO: change this to render emoji with emoji id "key" */}
-                id {key}: {value}
+                {renderEmoji(key)} {count}
             </button>
         );
     }
@@ -25,9 +26,27 @@ const reduceReactions = (reactions) => {
     return buttons;
 };
 
+// TODO: refactor into a list in a diff file maybe
+const renderEmoji = (id) => {
+    return [
+        <span>&#128512;</span>,
+        <span>&#128513;</span>,
+        <span>&#128514;</span>,
+        <span>&#128515;</span>,
+        <span>&#128516;</span>,
+        <span>&#128517;</span>,
+        <span>&#128518;</span>,
+        <span>&#128519;</span>,
+        <span>&#128520;</span>,
+        <span>&#128521;</span>,
+        <span>&#128522;</span>,
+    ][id];
+};
+
 const Chat = ({ initMessages, channelId }) => {
     const [chatInput, setChatInput] = useState("");
     const [messages, setMessages] = useState(initMessages);
+    const [showReactionsMenu, setShowMenu] = useState(false);
     const user = useSelector((state) => state.session.user);
     const messagesEndRef = useRef(null);
     const messagesContainerRef = useRef(null);
@@ -105,7 +124,6 @@ const Chat = ({ initMessages, channelId }) => {
                     <div className="messages-wrapper">
                         {messages.map((message, ind) => (
                             <div className="message-container" key={ind}>
-                                {console.log(message)}
                                 <div className="profile-pic-container">
                                     {message?.user?.imageUrl && (
                                         <img
@@ -124,18 +142,31 @@ const Chat = ({ initMessages, channelId }) => {
                                     <div className="message-text">
                                         <p>{message.message}</p>
                                     </div>
+                                    {message.reactions.length > 0 && (
+                                        <div className="reactions">
+                                            {message.reactions.length > 0 &&
+                                                reduceReactions(
+                                                    message.reactions
+                                                )}
+                                        </div>
+                                    )}
                                 </div>
-                                <div className="message-mamangement-container">
-                                    <div className="reactions">
-                                        {message.reactions.length > 0 &&
-                                            reduceReactions(message.reactions)}
-                                    </div>
+                                <div className="message-management-container">
+                                    <button
+                                        className="reactions"
+                                        onClick={() =>
+                                            setShowMenu(!showReactionsMenu)
+                                        }
+                                    >
+                                    </button>
                                     <button className="edit"></button>
                                     <button className="delete"></button>
                                 </div>
                             </div>
                         ))}
                         <div ref={messagesEndRef}></div>
+                        {/* TODO: make buttons add reactions CHRIS MORNING */}
+                        {showReactionsMenu && <ReactionsList />}
                     </div>
                 </div>
 
