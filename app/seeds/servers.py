1ee17a7586a8fe3784bda705056d8d5a3b659867
cpeter41 <chris.steven.peters@gmail.com>
2024-05-28 07:07:20 -0700

    seed data

--- app/seeds/servers.py
+++ app/seeds/servers.py
@@ -7,31 +7,31 @@ def seed_servers():
             name='MineCraft 4EVERRRRR',
             creatorId=1,
             description='Lovers of Minecraft rejoice!!!',
-            imageUrl='https://images.pexels.com/photos/1670977/pexels-photo-1670977.jpeg'
+            imageUrl='https://static.wikia.nocookie.net/minecraft/images/f/fe/GrassNew.png/revision/latest/scale-to-width/360?cb=20190903234415'
         ),
         Server(
             name='League of Legends Forever',
-            creatorId=2,
+            creatorId=1,
             description='Join us for epic battles in LoL!',
-            imageUrl='https://images.pexels.com/photos/1763434/pexels-photo-1763434.jpeg'
+            imageUrl='https://i.scdn.co/image/ab6761610000e5ebe80d1ffb81aa6503ad41c574'
         ),
         Server(
             name='Among Us Party',
-            creatorId=3,
+            creatorId=1,
             description='Find the impostor among us!',
-            imageUrl='https://images.pexels.com/photos/4388160/pexels-photo-4388160.jpeg'
+            imageUrl='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3p2zjGV5bKEMt2F3niChdG2BbLV0TmQcLySeexXhmOg&s'
         ),
         Server(
             name='Fortnite Fanatics',
             creatorId=1,
             description='Drop in and join the Fortnite frenzy!',
-            imageUrl='https://images.pexels.com/photos/1693095/pexels-photo-1693095.jpeg'
+            imageUrl='https://yt3.googleusercontent.com/o5_XZSJFSkVJ4cRUIOGkuM6aTqoF1YjTlxsZ--IJyDhQHUGIr7w9DJK2iz7qhBnYGfS2Fv5p=s900-c-k-c0x00ffffff-no-rj'
         ),
         Server(
             name='Call of Duty Crew',
-            creatorId=2,
+            creatorId=1,
             description='Join us for some CoD action!',
-            imageUrl='https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg'
+            imageUrl='https://yt3.googleusercontent.com/Wz_UJUsKf5PQzlWBhSf0VfshMQbahqDf6byz2uXcSEp9L5nseaCqiYWoagrYjDs3tIOVUqbYRSM=s900-c-k-c0x00ffffff-no-rj'
         )
     ]
 
